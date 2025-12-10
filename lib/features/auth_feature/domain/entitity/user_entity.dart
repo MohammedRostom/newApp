a25a77c5d22e0c3fad7a4edca9ceddeeba907c5e
MohammedRostom <mohammedrostom011@gmail.com>
2025-12-10 13:46:07 +0200

    AuthUserEntity  encapsualtion

---
+++ lib/features/auth_feature/domain/entitity/user_entity.dart
@@ -0,0 +1,19 @@
+class AuthUserEntity {
+  final String _id;
+  final String _email;
+  final String _username;
+
+  AuthUserEntity({
+    required String id,
+    required String email,
+    required String username,
+  }) : _id = id,
+       _email = email,
+       _username = username;
+
+  String get id => _id;
+  String get email => _email;
+  String get username => _username;
+}
+
+//applay encapsualtion Done
