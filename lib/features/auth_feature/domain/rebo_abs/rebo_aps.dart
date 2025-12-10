dcfe8fafbfd1cd49b3ef591498c920f7ce80bf8b
MohammedRostom <mohammedrostom011@gmail.com>
2025-12-10 14:14:14 +0200

    some edits

--- lib/features/auth_feature/domain/rebo_abs/rebo_aps.dart
+++ lib/features/auth_feature/domain/rebo_abs/rebo_aps.dart
@@ -1,4 +1,4 @@
-import 'package:auth_feature_1_0/features/auth_feature/domain/entitity/user_entity.dart';
+import 'package:auth_feature_1_0/features/auth_feature/Domain/entitity/user_entity.dart';
 
 abstract class RepositoryAbs {
   Future<AuthUserEntity> login(String email, String password);
